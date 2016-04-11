a9a9f880a66914408a668eab10ec8d22d7ca5959
vbenavente <vbenavente@hotmail.com>
2016-04-11 14:00:16 -0700

    Article object constructor complete

--- class-01-jquery-and-the-dom/pair-assignment/victor-david/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/victor-david/scripts/article.js
@@ -1,9 +1,14 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
+  // DONE: Use the js object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
