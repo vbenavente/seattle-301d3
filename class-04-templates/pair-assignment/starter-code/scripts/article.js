aa2126169ce0d5befe4860424ab6ab4b4a8d9f6b
vbenavente <vbenavente@hotmail.com>
2016-04-14 13:02:54 -0700

    converted template html into handlebars style template and added expressions

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -10,7 +10,10 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  var $source = $('#blog-template').html();
+  var template = Handlebars.compile($source);
+  
+  // DONE: Use handlebars to render your articles.
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
@@ -21,7 +24,8 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
 };
 
 rawData.sort(function(a,b) {
