85ff9aa453879752820a8756e056eccff5df0266
vbenavente <vbenavente@hotmail.com>
2016-04-25 12:41:04 -0700

    added code to show about section

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -2,8 +2,10 @@
   var aboutController = {};
 
   aboutController.index = function() {
-    // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+    $('main > section').hide();
+    $('#about').show();
   };
+    // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
 
   module.aboutController = aboutController;
 })(window);
