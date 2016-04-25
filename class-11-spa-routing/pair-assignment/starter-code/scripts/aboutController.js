c1564dd089143e44795cc85c6b7c308815482279
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-25 12:45:00 -0700

    added functionality for routes file

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -5,7 +5,7 @@
     $('main > section').hide();
     $('#about').show();
   };
-    // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+    // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
 
   module.aboutController = aboutController;
 })(window);
--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,6 @@
-// TODO: Configure our two routes for this app with page.js, by registering
+// DONE: Configure our two routes for this app with page.js, by registering
 //  each URL your app can handle, linked to a single controller function to handle it:
-
-// TODO: What function do we call to activate page.js? Fire it off now, to execute it:
+page('/', articlesController.index);
+page('/about', aboutController.index);
+// DONE: What function do we call to activate page.js? Fire it off now, to execute it:
+page();
